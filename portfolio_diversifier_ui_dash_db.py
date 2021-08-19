593a9e04d64b29dcb74dab21b015f38e2829a864
dylanbowsky <bowsky23@gmail.com>
2021-08-18 22:38:12 -0700

    added new tab for cumulative return and graphs for forcasting and cumulative returns

--- portfolio_diversifier_ui_dash_db.py
+++ portfolio_diversifier_ui_dash_db.py
@@ -1,12 +1,14 @@
 import dash
 import dash_core_components as dcc
+from dash_core_components.Dropdown import Dropdown
+from dash_core_components.Graph import Graph
 import dash_html_components as html
 import pandas as pd
 import plotly.express as px
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
-
-
+import dash_table
+from MCForecastTools import plot_simulation
 #Saved Data Files from CSV or DataFrame need to add some sample data for visual help
 #CSV file need help with the explanation of each
 data_tickers = pd.read_csv("ticker_data.csv")
@@ -19,6 +21,7 @@ data_tickers.sort_index(inplace=True)
 risk_return = pd.read_csv("risk_ret.csv")
 column_names = ['Tickers','Start Date','End Date','WARP','+Sortino','+Ret_To_MaxDD','Sharpe','Sortino','Max_DD']
 risk_return.columns = column_names
+
 #print(risk_return)
 
 
@@ -37,23 +40,47 @@ new_ports= new_ports.transpose()
 
 
 #Plotly Graphs 
-fig_bar_total = px.bar(risk_return,
- x="Tickers",y=["WARP",'+Sortino','+Ret_To_MaxDD','Sharpe','Sortino','Max_DD',],
-  hover_data=["Tickers"],title="Risk and return looking at all risk ratios", template='plotly_dark')
-
-fig_bar_sharpe = px.bar(risk_return,
- x="Tickers",y='Sharpe',
-  hover_data=["Tickers"], title="Risk and return looking at only Sharpe Ratio")
-
+risk_return.sort_values('WARP', inplace=True, ascending=False)
 fig_bar_warp = px.bar(risk_return,
  x="Tickers",y='WARP',
   hover_data=["Tickers"],
    title="Risk and return looking at only WARP Ratio", template='plotly_dark')
+risk_return.sort_values('Sharpe', inplace=True, ascending=False)
+fig_bar_sharpe = px.bar(risk_return,
+ x="Tickers",y='Sharpe',
+  hover_data=["Tickers"], title="Risk and return looking at only Sharpe Ratio")
 
+risk_return.sort_values('Sortino', inplace=True, ascending=False)
 fig_bar_sortino = px.bar(risk_return,
  x="Tickers",y='Sortino',
   hover_data=["Tickers"],
-   title="Risk and return looking at Sortino Ratio")
+   title="Risk and return looking at Sortino Ratio", template='plotly_dark')
+
+#Cumulative Returns
+cumulative_total= pd.read_csv('cumulative_returns_selected.csv',index_col='Date')
+fig_cumulative_total = px.line(cumulative_total,
+ title='Cumulative Returns since 2008 to 2020', template='plotly_dark')
+
+cumulative_bull= pd.read_csv('cumulative_returns_selected_2010_2019.csv',index_col='Date')
+fig_cumulative_bull=  px.line(cumulative_bull,
+ title='Cumulative returns would be in a bull market 10-19')
+
+cumulative_bear= pd.read_csv('cumulative_returns_selected_2008_2009.csv', index_col='Date')
+fig_cumulative_bear= px.line(cumulative_bear, title='Cumulative Returns during a bear market 08-09', template='plotly_dark')
+
+#MonteCarlo Forcasting
+mc_gld = pd.read_csv('monte_carlo_simulation_table_gld.csv')
+fig_mc_gld = mc_gld.plot_simulation()
+
+mc_shy = pd.read_csv('monte_carlo_simulation_table_shy.csv')
+fig_mc_shy = mc_shy.plot_simulation()
+
+mc_tlt= pd.read_csv('monte_carlo_simulation_table_tlt.csv')
+fig_mc_tlt = mc_tlt.plot_simulation()
+
+
+
+
 
 
 #Creating the style of the Dashboard currently using a simple tab measure
@@ -91,19 +118,25 @@ app.layout = html.Div(id="output_container",
         dcc.Tabs(id='tabs', value="tab-1",
          children=[
              dcc.Tab(label="Risk Ratio Graphs and Data", children=[
-                 dcc.Graph(id="line_graph_ratio", figure=fig_bar_total
+                 dcc.Graph(id="line_graph_ratio", figure=fig_bar_warp
         ),
         html.Br(),
         dcc.Graph(id="sharpe_ratio",
             figure=fig_bar_sharpe),
         dcc.Graph(
-            figure=fig_bar_warp,
-        ),
-        dcc.Graph(
             figure=fig_bar_sortino),
             ] ),
-            dcc.Tab(label="Forcasting Data", value='tab-2'),]), 
-            html.Div('tabs_selection'),
+            dcc.Tab(label='Forecasting', value= 'tab-2', children=[
+                 dcc.Graph(figure=fig_mc_gld),
+                 dcc.Graph(figure=fig_mc_shy),
+                 dcc.Graph(figure=fig_mc_tlt)
+            ]),
+            dcc.Tab(label='Cumulative', value='tab-3', children=[
+                dcc.Graph(figure=fig_cumulative_total),
+                dcc.Graph(figure=fig_cumulative_bull),
+                dcc.Graph(figure=fig_cumulative_bear),
+        ]),
+    ])
          
     ]
 )
@@ -119,6 +152,10 @@ def render_content(tab):
     elif tab == 'tab-2':
         return html.Div([
             html.H3('Tab content 2')
+            ])
+    elif tab == 'tab-3':
+        return html.Div([
+            html.H3('Tab content 3')
         ])
  
  
